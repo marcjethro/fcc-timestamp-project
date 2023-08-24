6430e068e03f50ab26b2ef723a1f1ce70767bc13
marcjethro <marcjethro@gmail.com>
2023-08-25 01:00:49 +0800

    Added /api/:date?

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", (req, res) => {
+  res.json({error: "Invalid Date"});
 });
 
 
